4bad4a6efdeabaa779a11ede89ad0db3d6982ec9
Matt Eaton <agnosticdev@gmail.com>
2017-07-20 05:40:10 -0500

    Update Contents.swift
    Edited final range information

--- OneSidedRanges.playground/Contents.swift
+++ OneSidedRanges.playground/Contents.swift
@@ -1,10 +1,6 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-
-// Swift 3 information
-// https://stackoverflow.com/questions/39676939/how-does-string-index-work-in-swift-3
-
 // Swift 4 information
 // https://github.com/apple/swift-evolution/blob/master/proposals/0172-one-sided-ranges.md
 
@@ -17,17 +13,28 @@ guard let i = str.index(of: ",") else {
     throw "Not safe to continue on! 😎"
 }
 
+// ###### Start Index Inferred ######
+
 // First slice with new range implementation for Swift 4, a lot less verbose
-let firstSliceWithComma = str[...i] // Hello,
-let firstSliceUptoComma = str[..<i] // Hello
+let firstSliceWithComma = str[...i] // Hello, -  a lot less verbose
+let firstSliceUptoComma = str[..<i] // Hello -  a lot less verbose
 
 // Swift 3 implementation, very similar, but on two lines
 let firstSliceRangeUptoComma = str.startIndex..<i
 str[firstSliceRangeUptoComma] // Hello
+
+// or
+
+str[str.startIndex..<i] // Hello -  a lot more verbose
+
 let firstSliceRangeWithComma = str.startIndex...i
 str[firstSliceRangeWithComma] // Hello,
 
+// or
 
+str[str.startIndex...i] // Hello, -  a lot more verbose
+
+// ###### End Index Inferred ######
 
 // Second slice with new range implementation for Swift 4
 let secondSliceWithComma = str[i...] //, One-sided Ranges!
@@ -35,7 +42,5 @@ let secondSliceWithComma = str[i...] //, One-sided Ranges!
 // Swift 3 implementation, very similar, but on two lines
 let secondSliceRangeWithComma = i..<str.endIndex
 str[secondSliceRangeWithComma]
-
-
 
 
